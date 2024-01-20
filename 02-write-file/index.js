49222b4d49fb6ac892e3e1573981fad94ac068d0
Habrhammer <iamsorry1885@gmail.com>
2024-01-20 21:27:17 +0100

    feat: writing console input to file

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const stream = fs.createWriteStream(`${__dirname}/text.txt`);
+
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+process.stdout.write('Hello!');
+
+readlineInterface.on('line', (input) => {
+  if (input === 'exit') {
+    readlineInterface.close();
+    stream.end()
+  } else {
+    stream.write(input + '\n');
+  }
+});
+
+process.on('exit', () => {
+  process.stdout.write("Goodbye!");
+});
